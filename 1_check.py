636fd2e32a2d2d5a36b9b7e66a1bc96733e9be5a
Mike Semple <mwsemple@me.com>
2015-09-21 19:55:59 +0000

    typo

--- 1_check.py
+++ 1_check.py
@@ -9,9 +9,10 @@ def camera_1():
   #post data should look like '{"plate":"YS54 GBF","time": 1442862678}'
   rawpostdata = request.body.read()
   try:
-    postdata = json.loads(rawpostdata.decode())
-  except ValueError:
+    postdata = json.loads(rawpostdata.decode()) #had to decode cause of problem https://stackoverflow.com/questions/24069197/httpresponse-object-json-object-must-be-str-not-bytes
+  except:
     return '{"error":"True"}'
+  return postdata
   if re.match("/\b[a-z]{2}([1-9]|0[2-9]|6[0-9]|1[0-9])[a-z]{3}\b/i ", postdata[plate], flags=0):
     return '{"error":"False","stored":"true"}'
 run(host='0.0.0.0', port=8080)
