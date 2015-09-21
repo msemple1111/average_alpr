e1ad6069adc411dcfd1413a22241e2b2a06bd96f
Mike Semple <mwsemple@me.com>
2015-09-21 20:36:33 +0000

    adding regex2

--- 1_check.py
+++ 1_check.py
@@ -12,7 +12,15 @@ def camera_1():
     postdata = json.loads(rawpostdata.decode()) #had to decode cause of problem https://stackoverflow.com/questions/24069197/httpresponse-object-json-object-must-be-str-not-bytes
   except:
     return '{"error":"True"}'
-  if re.match("/\b[a-z]{2}([1-9]|0[2-9]|6[0-9]|1[0-9])[a-z]{3}\b/i ", postdata['plate']):
+  postdata['plate'].replace(" ", "")
+  1_plate = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
+  2_plate = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
+  3_plate = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
+  4_plate = re.compile("[0-9]{1,4}[A-Z]{1,2}")
+  5_plate = re.compile("[0-9]{1,3}[A-Z]{1,3}")
+  6_plate = re.compile("[A-Z]{1,2}[0-9]{1,4}")
+  7_plate = re.compile("[A-Z]{1,3}[0-9]{1,3}")
+  if 1_plate.match() or 2_plate.match() or 3_plate.match() or 4_plate.match() or 5_plate.match() or 6_plate.match() or 7_plate.match():
     return '{"error":"False","stored":"True"}'
   else:
     return '{"error":"True","stored":"False"}'
