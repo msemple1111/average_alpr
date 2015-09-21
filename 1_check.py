d917f9dc34bb5c44d599936e2e385772981ac887
Mike Semple <mwsemple@me.com>
2015-09-21 19:39:30 +0000

    second commit

--- 1_check.py
+++ 1_check.py
@@ -3,9 +3,19 @@ import json
 from bottle import route, run, request
 
 
-@route('/api/average_speed', method='POST')
-def average_speed():
-  postdata = request.body.read()
-   return 
-
+@route('/api/camera/1', method='POST')
+def camera_1():
+  #post data should look like '{"plate":"YS54 GBF","time": 1442862678}'
+  try:
+    postdata = json.loads(request.body.read())
+  except ValueError, e:
+    return '{"error":"True"}'
+  if re.match("/\b[a-z]{2}([1-9]|0[2-9]|6[0-9]|1[0-9])[a-z]{3}\b/i ", postdata[plate], flags=0):
+    return '{"error":"False","stored":"true"}'
 run(host='0.0.0.0', port=8080)
+
+/\b[a-z]{2}([1-9]|0[2-9]|6[0-9]|1[0-9])[a-z]{3}\b/i      # current series
+/\b[A-HJ-NP-Y]\d{1,3}[A-Z]{3}\b/        # previous series
+/\b[A-Z]{3}\d{1,3}[A-HJ-NP-Y]\b/        # previous series
+/\b(?:[A-Z]{1,2}\d{1,4}|[A-Z]{3}\d{1,3})\b/     # old series - letters first
+/\b(?:\d{1,4}[A-Z]{1,2}|\d{1,3}[A-Z]{3})\b/     # old series - digits first
