f769b9ca3c65acf583f27396c9a69d9c05cc412a
Mike Semple <mwsemple@me.com>
2015-09-21 20:38:17 +0000

    adding regex2 typo

--- 1_check.py
+++ 1_check.py
@@ -13,14 +13,14 @@ def camera_1():
   except:
     return '{"error":"True"}'
   postdata['plate'].replace(" ", "")
-  1_plate = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
-  2_plate = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
-  3_plate = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
-  4_plate = re.compile("[0-9]{1,4}[A-Z]{1,2}")
-  5_plate = re.compile("[0-9]{1,3}[A-Z]{1,3}")
-  6_plate = re.compile("[A-Z]{1,2}[0-9]{1,4}")
-  7_plate = re.compile("[A-Z]{1,3}[0-9]{1,3}")
-  if 1_plate.match() or 2_plate.match() or 3_plate.match() or 4_plate.match() or 5_plate.match() or 6_plate.match() or 7_plate.match():
+  plate_1 = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
+  plate_2 = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
+  plate_3 = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
+  plate_4 = re.compile("[0-9]{1,4}[A-Z]{1,2}")
+  plate_5 = re.compile("[0-9]{1,3}[A-Z]{1,3}")
+  plate_6 = re.compile("[A-Z]{1,2}[0-9]{1,4}")
+  plate_7 = re.compile("[A-Z]{1,3}[0-9]{1,3}")
+  if plate_1.match() or plate_2.match() or plate_3.match() or plate_4.match() or plate_5.match() or plate_6.match() or plate_7.match():
     return '{"error":"False","stored":"True"}'
   else:
     return '{"error":"True","stored":"False"}'
