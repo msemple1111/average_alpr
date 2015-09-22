b3a8e68c34bc18a2d29e92bd7b3a5b69ba980f61
Mike Semple <mwsemple@me.com>
2015-09-22 17:15:05 +0000

    fixed issue with whitespace

--- 1_check.py
+++ 1_check.py
@@ -13,9 +13,10 @@ def camera_1():
   rawpostdata = request.body.read()
   try:
     postdata = json.loads(rawpostdata.decode()) #had to decode cause of problem https://stackoverflow.com/questions/24069197/httpresponse-object-json-object-must-be-str-not-bytes
+    plate = postdata['plate']
+    plate.replace(" ", "")
   except:
     return '{"error":"True"}'
-  postdata['plate'].replace(" ", "")
   plate_1 = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
   plate_2 = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
   plate_3 = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
@@ -23,13 +24,13 @@ def camera_1():
   plate_5 = re.compile("[0-9]{1,3}[A-Z]{1,3}")
   plate_6 = re.compile("[A-Z]{1,2}[0-9]{1,4}")
   plate_7 = re.compile("[A-Z]{1,3}[0-9]{1,3}")
-  if plate_1.match(postdata['plate']) or plate_2.match(postdata['plate']) or plate_3.match(postdata['plate']) or plate_4.match(postdata['plate']) or plate_5.match(postdata['plate']) or plate_6.match(postdata['plate']) or plate_7.match(postdata['plate']):
-    if rdb.execute("select (1) from plates where plate = '"+postdata['plate']+"' limit 1;"): 
-      return '{"error":"False","stored":"True"}'+postadata['plate']
+  if plate_1.match(plate) or plate_2.match(plate) or plate_3.match(plate) or plate_4.match(plate) or plate_5.match(plate) or plate_6.match(plate) or plate_7.match(plate):
+    if rdb.execute("select (1) from plates where plate = '"+plate+"' limit 1;"): 
+      return '{"error":"False","stored":"True"}'+plate
     else:
-      return '{"error":"False","stored":"False1"}'+postadata['plate']
+      return '{"error":"False","stored":"False1"}'+plate
   else:
-    return '{"error":"True","stored":"False"}'+postadata['plate']
+    return '{"error":"True","stored":"False"}'
 run(host='0.0.0.0', port=8080)
 
 '''
