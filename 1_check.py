dfaaa8d2d561b3e5b22f14d0923610674e5092eb
Mike Semple <mwsemple@me.com>
2015-10-14 21:08:29 +0000

    re compile list update

--- 1_check.py
+++ 1_check.py
@@ -14,21 +14,22 @@ def camera_1():
   try:
     postdata = json.loads(rawpostdata.decode()) #had to decode cause of problem https://stackoverflow.com/questions/24069197/httpresponse-object-json-object-must-be-str-not-bytes
     plate = postdata['plate']
-    plate.replace(" ", "")
   except:
     return '{"error":"True"}'
-  plate_1 = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
-  plate_2 = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
-  plate_3 = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
-  plate_4 = re.compile("[0-9]{1,4}[A-Z]{1,2}")
-  plate_5 = re.compile("[0-9]{1,3}[A-Z]{1,3}")
-  plate_6 = re.compile("[A-Z]{1,2}[0-9]{1,4}")
-  plate_7 = re.compile("[A-Z]{1,3}[0-9]{1,3}")
-  if plate_1.match(plate) or plate_2.match(plate) or plate_3.match(plate) or plate_4.match(plate) or plate_5.match(plate) or plate_6.match(plate) or plate_7.match(plate):
+  re_plate[1] = re.compile("[A-Z]{2}[0-9]{2}[A-Z]{3}") #https://gist.github.com/danielrbradley/7567269
+  re_plate[2] = re.compile("[A-Z][0-9]{1,3}[A-Z]{3}")
+  re_plate[3] = re.compile("[A-Z]{3}[0-9]{1,3}[A-Z]")
+  re_plate[4] = re.compile("[0-9]{1,4}[A-Z]{1,2}")
+  re_plate[5] = re.compile("[0-9]{1,3}[A-Z]{1,3}")
+  re_plate[6] = re.compile("[A-Z]{1,2}[0-9]{1,4}")
+  re_plate[7] = re.compile("[A-Z]{1,3}[0-9]{1,3}")
+  
+  if re_plate[1].match(plate) or re_plate[2].match(plate) or re_plate[3].match(plate) or re_plate[4].match(plate) or re_plate[5].match(plate) or re_plate[6].match(plate) or re_plate[7].match(plate):
     if rdb.execute("select (1) from plates where plate = '"+plate+"' limit 1;"): 
-      return '{"error":"False","stored":"True"}'+plate
+      lol = str(rdb.execute("select p_index from plates where plate = 'YS54 GBF';"))
+      return lol
     else:
-      return '{"error":"False","stored":"False1"}'+plate
+      return '{"error":"True","stored":"False1"}'+plate
   else:
     return '{"error":"True","stored":"False"}'
 run(host='0.0.0.0', port=8080)
