9f919ed80ed20c8eb31fde365d967a47737de0d8
mike semple <mwsemple@me.com>
2015-12-05 12:26:26 +0000

    added an create.sql file

---
+++ create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE plates (
+  p_index INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  plate VARCHAR(15) NOT NULL,
+  p_foreign BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE data (
+   d_index INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+   p_index INTEGER NOT NULL,
+   r_id INTEGER,
+   time_1 BIGINT NOT NULL,
+   time_2 BIGINT,
+   speed float(5,2)
+ );
+
+CREATE TABLE roads (
+  r_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  r_name VARCHAR(255) NOT NULL,
+  s_limit float(5,2) NOT NULL,
+  r_dist float(8,2) NOT NULL
+ );
