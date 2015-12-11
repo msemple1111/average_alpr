5af8bf2c1eebbb126b72d555764c074b3d560b32
mike semple <mwsemple@me.com>
2015-12-11 21:53:31 +0000

    added classes in webserver.py and removed unneeded functions

--- webserver.py
+++ webserver.py
@@ -28,63 +28,11 @@ def internal_error_500(error):
 def hello():
   return "<h1 style='color:blue'>Hello There!,,G</h1>"
 
+
 @application.route('/api/camera', methods=['POST'])
-def cam():
-  request_data = request.json
-  valid = validate()
-  site_id = valid.site(request_data['site_id'])
-  cam_id = valid.cam(request_data['camera_id'])
-  uuid = valid.uuid(request_data['uuid'])
-  time_2 = valid.time(request_data['epoch_time'])
-  sqlite = database()
-  calc = calculate()
-  s_id, s_limit = sqlite.find_site(site_id)
-  curr_cam_m = sqlite.get_cam_m(cam_id, s_id)
-  for plate_no in range(0,len(request_data['results'])):
-    plate, p_forign = valid.plate(request_data['results'][plate_no]['plate'])
-    p_confidence = valid.confidence(request_data['results'][plate_no]['confidence'])
-    p_id = sqlite.add_plate(plate, p_forign)#
-    cam_first = sqlite.cam_first(curr_cam_m, time_2, p_id, s_id)
-    if cam_first:
-      sqlite.record_first(p_id, cam_id, s_id, uuid, time_2)
-    else:
-      time_1, prev_cam_id = sqlite.last_cam(p_id, s_id)#
-      prev_cam_m = sqlite.get_cam_m(prev_cam_id, s_id)
-      r_dist = prev_cam_m - curr_cam_m
-      car_speed = calc.average_speed(time_2, time_1, r_dist)
-      speed = calc.speed_increase(car_speed, s_limit)
-      sqlite.record_time(p_id, cam_id, s_id, uuid, time_2, speed)#
-  #log p_id to sqlite data with r_id
-  #post data should look like (in any order) '{"road":1, "plate":"YS54 GBF", "time": 1442862678}'
-                                             #(road id,   number plate,      time in unix epoch)
+def main():
+  start = cam(request.json)
   return json.dumps({'error': False})
-
-@application.route('/api/camera/2', methods=['POST'])
-def cam_2():
-  
-  valid = validate()
-  road_id = valid.road(request.json['road'])#validating the road number input and then assinging it to a varible
-  p_name, p_forign = valid.plate(request.json['plate']) #validating the number plate input, assinging it to a varible and then returning if it is forign or not
-  time_2 = valid.time(request.json['time']) #validating the 'time' input and then assing it to a varible
-  
-  sqlite = database() #initlising the database class
-  p_id = sqlite.add_plate(p_name, p_forign) #the plate id
-  r_id = sqlite.check_road_id(road_id) #the road id
-  
-  d_index = sqlite.get_d_index(p_id, r_id) #the data storage id
-  time_1 = sqlite.get_time_1(p_id, r_id) # time in sec
-  r_dist = sqlite.get_r_dist(r_id) #road distance in m
-  s_limit = sqlite.get_s_limit(r_id) #speed limit in m/s
-  
-  calc = calculate()
-  car_speed = calc.average_speed(time_1, time_2, r_dist)
-  speed = calc.speed_increase(car_speed, s_limit)
-  
-  sqlite.record_time_2(d_index, time_2, speed)
-  return jsonify({'error': False}) #return green light
-
-def http_error(error_no):
-  abort(error_no)
   
 if __name__ == '__main__':
   application.run(host='0.0.0.0', port=7000, debug=True)
