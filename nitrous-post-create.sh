1a9ef28a8fed4ec2bdcebafda1c0d8c3f05e4941
mike semple <mwsemple@me.com>
2016-03-25 12:44:04 +0000

    updated nitrous-post-create.sh

--- nitrous-post-create.sh
+++ nitrous-post-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# cd ~/code
-# virtualenv -p python3 avg_alpr
-# cd ~/code/avg_alpr
-# source bin/activate
-# pip install tornado flask requests urllib3
-# cat create.sql | sqlite3 average_check.db
+cd ~/code
+virtualenv -p python3 average_alpr
+cd ~/code/average_alpr
+source bin/activate
+pip install tornado flask requests urllib3
+cat create.sql | sqlite3 average_check.db
